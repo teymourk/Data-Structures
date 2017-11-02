6fe554963b0b4f3dc52a519c6f54927486e84c07
Kiarash Teymoury <teymourk@oregonstate.edu>
2017-11-02 00:55:58 -0700

    Initial Commit with home page done

---
+++ Practice/LinkedList.swift
@@ -0,0 +1,13 @@
+//
+//  LinkedList.swift
+//  Practice
+//
+//  Created by Kiarash Teymoury on 11/2/17.
+//  Copyright © 2017 Kiarash Teymoury. All rights reserved.
+//
+
+import UIKit
+
+class Node<T> {
+    
+}
---
+++ Practice/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Kiarash Teymoury on 11/2/17.
+//  Copyright © 2017 Kiarash Teymoury. All rights reserved.
+//
+
+import UIKit
+
+struct DataStructures {
+    var name: String
+    init(_ name: String) {
+        self.name = name
+    }
+}
+
+
+class ViewController: UITableViewController {
+
+    var dataStructuresArray: [DataStructures] {
+        let linkedList = DataStructures("Linked List")
+        let stack = DataStructures("Stack")
+        let queue = DataStructures("Queue")
+        return [linkedList,
+                stack, queue]
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Data Structures"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.view.backgroundColor = .white
+        self.tableView.register(DataStructuresCell.self, forCellReuseIdentifier: "CellID")
+    }
+}
+
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataStructuresArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath) as? DataStructuresCell
+        let dataStructure = dataStructuresArray[indexPath.item]
+            cell?.dataStructure = dataStructure
+        return cell ?? UITableViewCell()
+    }
+}
+
+class DataStructuresCell: UITableViewCell {
+    var nameLabel: UILabel = {
+        let label = UILabel()
+            label.textColor = .darkGray
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.translatesAutoresizingMaskIntoConstraints = false
+       return label
+    }()
+    
+    var dataStructure: DataStructures? {
+        didSet {
+            nameLabel.text = dataStructure?.name ?? ""
+        }
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        
+        addSubview(nameLabel)
+        
+        nameLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+}
